2ecc84cbf8ee0ef9be5ee6bc795028a7e3cc4735
justin-park-02 <parkof50302@gmail.com>
2025-12-23 22:34:55 +0900

    Add files via upload

---
+++ products.js
@@ -0,0 +1,30 @@
+const products = [
+  {
+    id: 1,
+    name: "Test Page",
+    price: "₩99,999",
+    img: "images/Test.jpg",
+    colors: "핑크",
+    sizes: "M / L / XL"
+  },
+{
+    id: 2,
+    name: "IAB STUDIO 맨투맨",
+    price: "₩30,000",
+    img: "images/iab.jpg",
+    colors: "블랙 / 아이보리",
+    sizes: "all size",
+    taobao: "https://item.taobao.com/item.htm?id=997562598826&mi_id=00001r_-WxHB0tQbsJB5uVnQyr5WSrJ6z5iZH9LcYJDxhlY&spm=a21xtw.29178619.0.0"
+  },
+{
+    id: 3,
+    name: "톰브라운 밀라노스티치 가디건",
+    price: "₩130,000",
+    img: "images/thomcardigan.jpg",
+    colors: "네이비 / 차콜",
+    sizes: "all size",
+    taobao: "https://item.taobao.com/item.htm?id=723374657366&mi_id=0000ttwTom6EIkfMA6wWuH5O9RlAuhgw_5oQshlwAi7xIok&spm=a21xtw.29178619.0.0"
+  }
+];
+
+console.log("products.js 정상 로드", products);
